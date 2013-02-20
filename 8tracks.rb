fed7f37e58c32563994b8f8c6906a3e7553356ef
Sahil Chelaramani <SahilC.2200@gmail.com>
2013-02-21 00:01:55 +0530

    Issue #1 fix

--- 8tracks.rb
+++ 8tracks.rb
@@ -5,7 +5,8 @@ puts "Input the full URL to the 8tracks mix"
 mixurl=gets.chomp
 path=mixurl[18..-1]
 user=mixurl[19..-1][/([a-zA-Z0-9_]*)/]
-mixesurl='http://8tracks.com/users/'+user+'/mixes.jsonp?api_key=&per_page=300'
+mix=path[user.length+2..-1]
+mixesurl='http://8tracks.com/users/'+user+'/mixes.jsonp?api_key=b5de95d329b4cbfc3605a3c46072b8601b997c5b&per_page=300'
 respr = Net::HTTP.get_response(URI.parse(mixesurl))
 datar = respr.body
 resultr=JSON.parse(datar)
@@ -17,10 +18,13 @@ if pathr.eql? path then
 end
 i+=1
 end
-url = 'http://8tracks.com/sets/460486803/play.jsonp?mix_id='+resultr["mixes"][i]["id"].to_s()+'&api_key='
+url = 'http://8tracks.com/sets/460486803/play.jsonp?mix_id='+resultr["mixes"][i]["id"].to_s()+'&api_key=b5de95d329b4cbfc3605a3c46072b8601b997c5b'
 resp = Net::HTTP.get_response(URI.parse(url))
 data = resp.body
 result=JSON.parse(data)
+directory_name = Dir::pwd + "/" + mix
+Dir::mkdir(directory_name)
+Dir.chdir(directory_name)
 while !result["set"]["at_end"] do
 	puts result["set"]["track"]["name"]+"-"+result["set"]["track"]["performer"]
 	start=result["set"]["track"]["url"][/http:\/\/(([a-zA-Z0-9]*)\.([a-zA-Z0-9]*))*/]
@@ -34,7 +38,7 @@ while !result["set"]["at_end"] do
    	end
 	end
 	puts "Done."
-	url = 'http://8tracks.com/sets/460486803/next.jsonp?mix_id='+resultr["mixes"][i]["id"].to_s()+'&api_key='
+	url = 'http://8tracks.com/sets/460486803/next.jsonp?mix_id='+resultr["mixes"][i]["id"].to_s()+'&api_key=b5de95d329b4cbfc3605a3c46072b8601b997c5b'
 	resp = Net::HTTP.get_response(URI.parse(url))
 	data = resp.body
 	result=JSON.parse(data)
